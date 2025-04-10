609b126633293f92fb8a2efcf72f188a9610603b
manoarazakamiadana <manoarazakamiadana@gmail.com>
2025-04-10 07:03:45 +0200

    feat: :zap: init api project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <main className="min-h-screen bg-[#333] flex justify-center items-center">
+      <div className="bg-cyan-500 shadow shadow-cyan-500 px-4 py-2 rounded">
+        <h1 className="text-white">Bonjour</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
